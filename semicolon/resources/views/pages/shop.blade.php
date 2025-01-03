d554ff5570fb6d07a05a4284500451d7514b84c8
shishir3d <shishirpoudel2061@gmail.com>
2025-01-03 20:21:19 +0545

    feat: add product cards

--- semicolon/resources/views/pages/shop.blade.php
+++ semicolon/resources/views/pages/shop.blade.php
@@ -1,3 +1,198 @@
 @extends('layouts.frontend.app')
 @section('content')
+<div class="">
+
+
+    <div className="productHeading" class="mt-40 text-4xl max-w-screen-xl flex flex-wrap items-center justify-left pl-20 mx-auto p-4">
+        OUR PRODUCTS
+    </div>
+
+    <div className="line1" class="max-w-screen-xl flex flex-wrap items-center justify-evenly mx-auto p-4">
+
+        <div class="cursor-pointer group rounded-lg size-80 flex pt-0 items-center flex-col pl-0 justify-center border-solid border-2 border-green-500 relative top-0 shadow-lg hover:ring-4 hover:ring-green-500 transition-shadow">
+            <div class="mt-8 w-2/3 h-1/2 border-solid border-0 border-black aspect-[8/3]">
+                <img src="{{asset('images/shop_images/wheat.jpg')}}">
+            </div>
+            <div class="flex w-full pl-14 pt-4">
+                <p class="text-2xl">Organic Wheat</p>
+            </div>
+            <div class="mt-2 w-2/3 h-1/4 border-solid border-0 border-black aspect-[8/3] flex-col justify-center items-center">
+                <p class="text-xl">Rs: 4000</p>
+                <p class="text-gray-500"> 140 sold | <span class="text-green-500">★★★★<span></p>
+            </div>
+            <!-- Hidden Buy Now Button -->
+            <div class="absolute ml-40 bottom-[-40px] opacity-0 transition-all duration-300 group-hover:bottom-5 group-hover:opacity-100">
+                <button class="bg-green-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-green-600">
+                    Buy Now
+                </button>
+            </div>
+        </div>
+
+        <div class="cursor-pointer group rounded-lg size-80 flex pt-0 items-center flex-col pl-0 justify-center border-solid border-2 border-green-500 relative top-0 shadow-lg hover:ring-4 hover:ring-green-500 transition-shadow">
+            <div class="mt-8 w-2/3 h-1/2 border-solid border-0 border-black aspect-[8/3]">
+                <img src="{{asset('images/shop_images/wheat.jpg')}}">
+            </div>
+            <div class="flex w-full pl-14 pt-4">
+                <p class="text-2xl">Organic Wheat</p>
+            </div>
+            <div class="mt-2 w-2/3 h-1/4 border-solid border-0 border-black aspect-[8/3] flex-col justify-center items-center">
+                <p class="text-xl">Rs: 4000</p>
+                <p class="text-gray-500"> 140 sold | <span class="text-green-500">★★★★<span></p>
+            </div>
+            <!-- Hidden Buy Now Button -->
+            <div class="absolute ml-40 bottom-[-40px] opacity-0 transition-all duration-300 group-hover:bottom-5 group-hover:opacity-100">
+                <button class="bg-green-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-green-600">
+                    Buy Now
+                </button>
+            </div>
+        </div>
+
+        <div class="cursor-pointer group rounded-lg size-80 flex pt-0 items-center flex-col pl-0 justify-center border-solid border-2 border-green-500 relative top-0 shadow-lg hover:ring-4 hover:ring-green-500 transition-shadow">
+            <div class="mt-8 w-2/3 h-1/2 border-solid border-0 border-black aspect-[8/3]">
+                <img src="{{asset('images/shop_images/wheat.jpg')}}">
+            </div>
+            <div class="flex w-full pl-14 pt-4">
+                <p class="text-2xl">Organic Wheat</p>
+            </div>
+            <div class="mt-2 w-2/3 h-1/4 border-solid border-0 border-black aspect-[8/3] flex-col justify-center items-center">
+                <p class="text-xl">Rs: 4000</p>
+                <p class="text-gray-500"> 140 sold | <span class="text-green-500">★★★★<span></p>
+            </div>
+            <!-- Hidden Buy Now Button -->
+            <div class="absolute ml-40 bottom-[-40px] opacity-0 transition-all duration-300 group-hover:bottom-5 group-hover:opacity-100">
+                <button class="bg-green-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-green-600">
+                    Buy Now
+                </button>
+            </div>
+        </div>
+
+
+    </div>
+
+    <div className="line2" class="max-w-screen-xl flex flex-wrap items-center justify-evenly mx-auto p-4">
+
+        <div class="cursor-pointer group rounded-lg size-80 flex pt-0 items-center flex-col pl-0 justify-center border-solid border-2 border-green-500 relative top-0 shadow-lg hover:ring-4 hover:ring-green-500 transition-shadow">
+            <div class="mt-8 w-2/3 h-1/2 border-solid border-0 border-black aspect-[8/3]">
+                <img src="{{asset('images/shop_images/wheat.jpg')}}">
+            </div>
+            <div class="flex w-full pl-14 pt-4">
+                <p class="text-2xl">Organic Wheat</p>
+            </div>
+            <div class="mt-2 w-2/3 h-1/4 border-solid border-0 border-black aspect-[8/3] flex-col justify-center items-center">
+                <p class="text-xl">Rs: 4000</p>
+                <p class="text-gray-500"> 140 sold | <span class="text-green-500">★★★★<span></p>
+            </div>
+            <!-- Hidden Buy Now Button -->
+            <div class="absolute ml-40 bottom-[-40px] opacity-0 transition-all duration-300 group-hover:bottom-5 group-hover:opacity-100">
+                <button class="bg-green-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-green-600">
+                    Buy Now
+                </button>
+            </div>
+        </div>
+
+        <div class="cursor-pointer group rounded-lg size-80 flex pt-0 items-center flex-col pl-0 justify-center border-solid border-2 border-green-500 relative top-0 shadow-lg hover:ring-4 hover:ring-green-500 transition-shadow">
+            <div class="mt-8 w-2/3 h-1/2 border-solid border-0 border-black aspect-[8/3]">
+                <img src="{{asset('images/shop_images/wheat.jpg')}}">
+            </div>
+            <div class="flex w-full pl-14 pt-4">
+                <p class="text-2xl">Organic Wheat</p>
+            </div>
+            <div class="mt-2 w-2/3 h-1/4 border-solid border-0 border-black aspect-[8/3] flex-col justify-center items-center">
+                <p class="text-xl">Rs: 4000</p>
+                <p class="text-gray-500"> 140 sold | <span class="text-green-500">★★★★<span></p>
+            </div>
+            <!-- Hidden Buy Now Button -->
+            <div class="absolute ml-40 bottom-[-40px] opacity-0 transition-all duration-300 group-hover:bottom-5 group-hover:opacity-100">
+                <button class="bg-green-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-green-600">
+                    Buy Now
+                </button>
+            </div>
+        </div>
+
+        <div class="cursor-pointer group rounded-lg size-80 flex pt-0 items-center flex-col pl-0 justify-center border-solid border-2 border-green-500 relative top-0 shadow-lg hover:ring-4 hover:ring-green-500 transition-shadow">
+            <div class="mt-8 w-2/3 h-1/2 border-solid border-0 border-black aspect-[8/3]">
+                <img src="{{asset('images/shop_images/wheat.jpg')}}">
+            </div>
+            <div class="flex w-full pl-14 pt-4">
+                <p class="text-2xl">Organic Wheat</p>
+            </div>
+            <div class="mt-2 w-2/3 h-1/4 border-solid border-0 border-black aspect-[8/3] flex-col justify-center items-center">
+                <p class="text-xl">Rs: 4000</p>
+                <p class="text-gray-500"> 140 sold | <span class="text-green-500">★★★★<span></p>
+            </div>
+            <!-- Hidden Buy Now Button -->
+            <div class="absolute ml-40 bottom-[-40px] opacity-0 transition-all duration-300 group-hover:bottom-5 group-hover:opacity-100">
+                <button class="bg-green-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-green-600">
+                    Buy Now
+                </button>
+            </div>
+        </div>
+
+
+    </div>
+
+    <div className="line3" class="max-w-screen-xl flex flex-wrap items-center justify-evenly mx-auto p-4">
+
+        <div class="cursor-pointer group rounded-lg size-80 flex pt-0 items-center flex-col pl-0 justify-center border-solid border-2 border-green-500 relative top-0 shadow-lg hover:ring-4 hover:ring-green-500 transition-shadow">
+            <div class="mt-8 w-2/3 h-1/2 border-solid border-0 border-black aspect-[8/3]">
+                <img src="{{asset('images/shop_images/wheat.jpg')}}">
+            </div>
+            <div class="flex w-full pl-14 pt-4">
+                <p class="text-2xl">Organic Wheat</p>
+            </div>
+            <div class="mt-2 w-2/3 h-1/4 border-solid border-0 border-black aspect-[8/3] flex-col justify-center items-center">
+                <p class="text-xl">Rs: 4000</p>
+                <p class="text-gray-500"> 140 sold | <span class="text-green-500">★★★★<span></p>
+            </div>
+            <!-- Hidden Buy Now Button -->
+            <div class="absolute ml-40 bottom-[-40px] opacity-0 transition-all duration-300 group-hover:bottom-5 group-hover:opacity-100">
+                <button class="bg-green-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-green-600">
+                    Buy Now
+                </button>
+            </div>
+        </div>
+
+        <div class="cursor-pointer group rounded-lg size-80 flex pt-0 items-center flex-col pl-0 justify-center border-solid border-2 border-green-500 relative top-0 shadow-lg hover:ring-4 hover:ring-green-500 transition-shadow">
+            <div class="mt-8 w-2/3 h-1/2 border-solid border-0 border-black aspect-[8/3]">
+                <img src="{{asset('images/shop_images/wheat.jpg')}}">
+            </div>
+            <div class="flex w-full pl-14 pt-4">
+                <p class="text-2xl">Organic Wheat</p>
+            </div>
+            <div class="mt-2 w-2/3 h-1/4 border-solid border-0 border-black aspect-[8/3] flex-col justify-center items-center">
+                <p class="text-xl">Rs: 4000</p>
+                <p class="text-gray-500"> 140 sold | <span class="text-green-500">★★★★<span></p>
+            </div>
+            <!-- Hidden Buy Now Button -->
+            <div class="absolute ml-40 bottom-[-40px] opacity-0 transition-all duration-300 group-hover:bottom-5 group-hover:opacity-100">
+                <button class="bg-green-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-green-600">
+                    Buy Now
+                </button>
+            </div>
+        </div>
+
+        <div class="cursor-pointer group rounded-lg size-80 flex pt-0 items-center flex-col pl-0 justify-center border-solid border-2 border-green-500 relative top-0 shadow-lg hover:ring-4 hover:ring-green-500 transition-shadow">
+            <div class="mt-8 w-2/3 h-1/2 border-solid border-0 border-black aspect-[8/3]">
+                <img src="{{asset('images/shop_images/wheat.jpg')}}">
+            </div>
+            <div class="flex w-full pl-14 pt-4">
+                <p class="text-2xl">Organic Wheat</p>
+            </div>
+            <div class="mt-2 w-2/3 h-1/4 border-solid border-0 border-black aspect-[8/3] flex-col justify-center items-center">
+                <p class="text-xl">Rs: 4000</p>
+                <p class="text-gray-500"> 140 sold | <span class="text-green-500">★★★★<span></p>
+            </div>
+            <!-- Hidden Buy Now Button -->
+            <div class="absolute ml-40 bottom-[-40px] opacity-0 transition-all duration-300 group-hover:bottom-5 group-hover:opacity-100">
+                <button class="bg-green-500 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-green-600">
+                    Buy Now
+                </button>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
+<div class="size-40"></div>
 @endsection
